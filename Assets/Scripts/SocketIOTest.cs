c59ac2fa770dc9fe5391482e2b814c8307b95086
LNWPOR <tclnwpor@gmail.com>
2017-01-18 17:36:08 +0700

    init SocketIO and TestSocketIO

---
+++ Assets/Scripts/SocketIOTest.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SocketIO;
+public class SocketIOTest : MonoBehaviour {
+	void Start () {
+        SocketOn();
+    }
+
+    private void SocketOn()
+    {
+        NetworkManager.Instance.Socket.On("NET_AVARIABLE", (SocketIOEvent evt) => {
+            Debug.Log("Net Avariable");
+        });
+    }
+}
