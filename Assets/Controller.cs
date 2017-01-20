990fa8a521ef141d992ff8b9a4d1f5e12ebaf760
Newtype1994 <aaatibet@hotmail.com>
2017-01-21 00:41:07 +0700

    Character

---
+++ Assets/Controller.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controller : MonoBehaviour
+{
+    public Rigidbody player;
+    public GameObject wave;
+    public float speed, waveSpeed;
+    int isMove;
+    public Color myColor;
+
+    void Start()
+    {
+        GetComponent<Renderer>().material.color = myColor;
+        Debug.Log(myColor);
+        isMove = 0;
+    }
+
+    void Update()
+    {
+        float z=0,x=0;
+        if (Input.GetKey("w"))
+        {
+            z = 1;
+        }
+        if (Input.GetKey("s"))
+        {
+            z = -1;
+        }
+        if (Input.GetKey("a"))
+        {
+            x = -1;
+        }
+        if (Input.GetKey("d"))
+        {
+            x = 1;
+        }
+        if (z != 0 || x != 0) isMove += 1;
+        else isMove = 0;
+        if (isMove == 20)
+        {
+            GameObject list;
+            Rigidbody a;
+            Renderer b;
+            list = Instantiate(wave, player.position, player.rotation);
+            a = list.GetComponent<Rigidbody>();
+            a.velocity = new Vector3(1, 0, 0)* waveSpeed;
+            b = list.GetComponent<Renderer>();
+            b.material.color = myColor;
+            list = Instantiate(wave, player.position, player.rotation);
+            a = list.GetComponent<Rigidbody>();
+            a.velocity = new Vector3(-1, 0, 0) * waveSpeed;
+            b = list.GetComponent<Renderer>();
+            b.material.color = myColor;
+            list = Instantiate(wave, player.position, player.rotation);
+            a = list.GetComponent<Rigidbody>();
+            a.velocity = new Vector3(0, 0, 1) * waveSpeed;
+            b = list.GetComponent<Renderer>();
+            b.material.color = myColor;
+            list = Instantiate(wave, player.position, player.rotation);
+            a = list.GetComponent<Rigidbody>();
+            a.velocity = new Vector3(0, 0, -1) * waveSpeed;
+            b = list.GetComponent<Renderer>();
+            b.material.color = myColor;
+            list = Instantiate(wave, player.position, player.rotation);
+            a = list.GetComponent<Rigidbody>();
+            a.velocity = new Vector3(1, 0, 1).normalized * waveSpeed;
+            b = list.GetComponent<Renderer>();
+            b.material.color = myColor;
+            list = Instantiate(wave, player.position, player.rotation);
+            a = list.GetComponent<Rigidbody>();
+            a.velocity = new Vector3(1, 0, -1).normalized * waveSpeed;
+            b = list.GetComponent<Renderer>();
+            b.material.color = myColor;
+            list = Instantiate(wave, player.position, player.rotation);
+            a = list.GetComponent<Rigidbody>();
+            a.velocity = new Vector3(-1, 0, 1).normalized * waveSpeed;
+            b = list.GetComponent<Renderer>();
+            b.material.color = myColor;
+            list = Instantiate(wave, player.position, player.rotation);
+            a = list.GetComponent<Rigidbody>();
+            a.velocity = new Vector3(-1, 0, -1).normalized * waveSpeed;
+            b = list.GetComponent<Renderer>();
+            b.material.color = myColor;
+        }
+        if(isMove > 20)
+        {
+            isMove += 1;
+            if (isMove == 40) isMove = 0;
+            player.velocity = new Vector3(0, 0, 0);
+        }
+        else
+        {
+            player.velocity = new Vector3(x, 0, z).normalized * speed;
+        }
+    }
+}
---
+++ Assets/Scripts/WaveLife.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveLife : MonoBehaviour {
+
+    float life;
+	void Start () {
+        life = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        life += 1;
+        GetComponent<Renderer>().material.color = new Color(GetComponent<Renderer>().material.color.r, GetComponent<Renderer>().material.color.g, GetComponent<Renderer>().material.color.b, 1f-(life/100));
+        if (life == 100)
+        {
+            Destroy(gameObject);
+        }
+	}
+}
