912e01ad1634267b6be4a32333f55afc8c77d419
Newtype1994 <aaatibet@hotmail.com>
2017-01-21 13:44:57 +0700

    Rotate

--- Assets/Controller.cs
+++ Assets/Controller.cs
@@ -45,7 +45,17 @@ public class Controller : Character {
     } else {
       player.velocity = new Vector3(x, 0, z).normalized * speed;
     }
-  }
+        
+        Plane playerPlane = new Plane(Vector3.up, transform.position);
+    	Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+    	float hitdist = 0.0f;
+    	if (playerPlane.Raycast (ray, out hitdist)) 
+		{
+        	Vector3 targetPoint = ray.GetPoint(hitdist);
+        	Quaternion targetRotation = Quaternion.LookRotation(targetPoint - transform.position);
+        	transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, speed * Time.deltaTime);
+		}
+    }
 
   void OnTriggerStay(Collider other) {
     if (other.gameObject.tag.Equals("Item"))
