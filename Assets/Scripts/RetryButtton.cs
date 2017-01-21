ccba64f81e908935e8a08f23fdb7105aa3f99117
LNWPOR <tclnwpor@gmail.com>
2017-01-22 06:54:04 +0700

    add retry button

---
+++ Assets/Scripts/RetryButtton.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RetryButtton : MonoBehaviour {
+
+	public void Retry()
+    {
+        SceneManager.LoadScene("Scene 3 test");
+    }
+}
